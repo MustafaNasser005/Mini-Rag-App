7b30b4204fb8a70f7294b586f849d52c6f3ef168
Mustafa Nasser <mustafa.nasser.002@gmail.com>
2026-09-21 01:54:23 +0300

    Fix data processing and Pydantic models

---
+++ src/models/db_schemes/project.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field, validator
+from typing import Optional
+from bson import ObjectId
+
+class Project(BaseModel):
+    id: Optional[ObjectId] = Field(default=None, alias="_id")
+    project_id: str = Field(..., min_length = 1)
+
+    @validator("project_id")
+    def valdite_project_id(cls, value):
+        if not value.isalnum():
+            raise ValueError("project_id must be alphanumeric")
+
+        return value
+
+    class Config:
+        arbitrary_types_allowed = True
